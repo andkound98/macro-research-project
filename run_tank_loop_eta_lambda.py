65bea278ea44a76a9d48d25374ce86dfee5f7f3d
Andreas Koundouros <koundouros.andreas@gmail.com>
2023-01-18 23:21:10 +0100

    small changes

--- run_tank_loop_eta_lambda.py
+++ run_tank_loop_eta_lambda.py
@@ -29,6 +29,7 @@ pio.renderers.default = "svg" # For plotting in the Spyder window
 percent = 100 # Turn to 100 (1) if impact effect should (not) be in percent
 varlist_consumption = 'c' # Specify variable to be checked (here: aggregate 
                           # consumption)
+impact = 1 # Choose time period of impact
 
 ###############################################################################
 ###############################################################################
@@ -71,13 +72,6 @@ lambda_sequence = np.arange(0.1, 0.46, 0.05)
 eta_sequence = np.arange(0.33, 1, 0.15)
 eta_sequence = np.append(eta_sequence, 1)
 
-# Convert sequnces to lists of strings for row and column names
-lambda_sequence_strings = lambda_sequence.tolist()
-lambda_sequence_strings = list(map(str, lambda_sequence_strings))
-
-eta_sequence_strings = eta_sequence.tolist()
-eta_sequence_strings = list(map(str, eta_sequence_strings))
-
 # Initialise empty container for impact values
 impact_eta_lambda = pd.DataFrame(np.nan, index = lambda_sequence, columns = eta_sequence)
 
@@ -96,10 +90,10 @@ for ee in eta_sequence:
     indx_rank_eta = [model_rank_eta['variables'].index(v) for v in varlist_consumption]
     rank_eta_index_c = indx_rank_eta[0]
     stst_rank_eta_c = x_rank_eta[-1, rank_eta_index_c]
-    impact_rank_eta_c = (x_rank_eta[1, rank_eta_index_c] - stst_rank_eta_c)/stst_rank_eta_c
+    impact_rank_eta_c = (x_rank_eta[impact, rank_eta_index_c] - stst_rank_eta_c)/stst_rank_eta_c
     
     for ll in lambda_sequence:
-        try:
+        try: # Some parameter combinations might not work
             # For a given eta, do TANK for lambda = ll
             tank_copy_eta_lambda['steady_state']['fixed_values']['eta'] = ee
             tank_copy_eta_lambda['steady_state']['fixed_values']['lam'] = ll
@@ -110,12 +104,12 @@ for ee in eta_sequence:
             indx_tank_eta_lambda = [model_tank_eta_lambda['variables'].index(v) for v in varlist_consumption]
             tank_eta_lambda_index_c = indx_tank_eta_lambda[0]
             stst_tank_eta_lambda_c = x_tank_eta_lambda[-1, tank_eta_lambda_index_c]
-            impact_tank_eta_lambda_c = (x_tank_eta_lambda[1, tank_eta_lambda_index_c] - stst_tank_eta_lambda_c)/stst_tank_eta_lambda_c
+            impact_tank_eta_lambda_c = (x_tank_eta_lambda[impact, tank_eta_lambda_index_c] - stst_tank_eta_lambda_c)/stst_tank_eta_lambda_c
             
             # Store results
             impact_eta_lambda.loc[ll, ee] = percent * (impact_tank_eta_lambda_c - impact_rank_eta_c)
         except:
-            continue
+            continue # Simply skip the parameter combinations that do not work
 
 ###############################################################################
 ###############################################################################
@@ -124,31 +118,34 @@ for ee in eta_sequence:
 
 newnames = {'0.33':'0.33', '0.48': '0.48', '0.6299999999999999': '0.63', 
             '0.7799999999999999': '0.78', '0.9299999999999999': '0.93', 
-            '1.0': '1.0'}
+            '1.0': '1.0'} # Correct column names
 fig = px.line(impact_eta_lambda, markers = True, 
-              color_discrete_sequence=['#636EFA', '#FFA15A', '#EF553B', 
-                                       '#00CC96', '#19D3F3', '#FECB52'],
+              color_discrete_sequence=px.colors.qualitative.Plotly[:len(eta_sequence)],
+              # ['#636EFA', '#FFA15A', '#EF553B', '#00CC96', '#19D3F3', '#FECB52']
               labels={'0.33': "hello"}) 
-fig.update_yaxes(range=[-4, 0.])
+fig.update_yaxes(range=[-4, 0.]) # Fix range of y-axis
 fig.update_traces(line=dict(width=3), 
                   marker=dict(size=10))
 fig.for_each_trace(lambda t: t.update(name = newnames[t.name],
                                       legendgroup = newnames[t.name],
-                                      hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])
-                                     )
-                  )
+                                      hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])))
 fig.update_layout(title='', # Empty title
                    xaxis_title="\u03BB", # x-axis labeling
                    yaxis_title='Relative Consumption Impact', # y-axis labeling
                    plot_bgcolor = 'whitesmoke', 
                    font=dict(size=20), 
-                   margin=dict(l=15, r=15, t=15, b=15), legend=dict(
+                   margin=dict(l=15, r=15, t=5, b=5), legend=dict(
     orientation="h", # For horizontal legend
     yanchor="bottom",
     y=1.02,
     xanchor="right",
     x=1
 ), legend_title=None)
-fig.show()
-fig.write_image("/Users/andreaskoundouros/Documents/Uni Master WS22:23/RM Macro/Project/Plots/sensitivity/sensitivity_technology.svg")
-#fig.write_image("/Users/andreaskoundouros/Documents/Uni Master WS22:23/RM Macro/Project/Plots/sensitivity/sensitivity_discount.svg")
+fig.show() # Display plot
+
+# Store plot
+if specific_shock[0] == 'e_z':
+    fig.write_image("/Users/andreaskoundouros/Documents/Uni Master WS22:23/RM Macro/Project/Plots/sensitivity/sensitivity_technology.svg")
+
+if specific_shock[0] == 'e_beta':
+    fig.write_image("/Users/andreaskoundouros/Documents/Uni Master WS22:23/RM Macro/Project/Plots/sensitivity/sensitivity_discount.svg")
